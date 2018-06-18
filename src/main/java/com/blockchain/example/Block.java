e527f95b0b4b6cb24897d3a7816e438944210cba
Sankara Telukutla <sankara.telukutla@gmail.com>
2018-06-17 17:27:46 -0700

    Fixed wrong parameters Added example to create 100 trasactions.

--- src/main/java/com/blockchain/example/Block.java
+++ src/main/java/com/blockchain/example/Block.java
@@ -3,19 +3,25 @@ package com.blockchain.example;
 import java.util.Date;
 
 /** Block Example */
+
 public class Block {
   public String hash;
   public String previousHash;
   private String data; // our data will be a simple message.
   private long timeStamp; // as number of milliseconds since 1/1/1970.
 
-  public Block(String previousHash, String data) {
+  public Block(String data, String previousHash) {
     this.data = data;
     this.previousHash = previousHash;
     this.timeStamp = new Date().getTime();
     this.hash = calculateSha256();
   }
 
+
+    /**
+     * Calcuate the hash using Sha256 algorithm
+     * @return
+     */
   public String calculateSha256() {
     String calculatedHash = StringUtils.applySha256(previousHash + Long.toString(timeStamp) + data);
     return calculatedHash;
--- src/main/java/com/blockchain/example/ReddyChain.java
+++ src/main/java/com/blockchain/example/ReddyChain.java
@@ -1,10 +1,19 @@
 package com.blockchain.example;
 
+/** Build the chain */
 public class ReddyChain {
   public static void main(String[] args) {
     Block firstBlock = new Block("Hi I am first block", "0");
     System.out.println("Hash for the first block :" + firstBlock.hash);
     Block secondBlock = new Block("Hi I am second Block", firstBlock.hash);
     System.out.println("Hash for the second block :" + secondBlock.hash);
+
+    // Create 100 blocks
+    String previousHash = secondBlock.hash;
+    for (int i = 0; i < 100; i++) {
+      Block dynamicBlock = new Block(" Block :" + i, previousHash);
+      System.out.println("Hash for the block : " + i + " ->" + dynamicBlock.hash);
+      previousHash = dynamicBlock.hash;
+    }
   }
 }
